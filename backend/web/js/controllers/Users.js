d1ea3ce3a8e5cc054adbebb2a73523c7aaf294ad
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-02-22 00:09:37 +0200

    modify user add

--- backend/web/js/controllers/Users.js
+++ backend/web/js/controllers/Users.js
@@ -1,30 +1,15 @@
-app.controller('UsersController', function ($scope,$timeout,$http,$window,$modal) {
+app.controller('UsersController', function ($scope,$timeout,$http,$window,$modal,$log) {
 
     $scope.openPopup = function () {
-        var modalInstance = $modal.open({
-            templateUrl: 'myModalContent.html',
-            controller: 'ModalInstanceCtrl',
+        var modal = $modal.open({
+            templateUrl: webroot + '/user/create',
 
-            //size: size,
-            resolve: {
-                user: function() {
-                    return userData;
-                },
-                selectedProducts: function() {
-                    return userData.selectedProducts;
-                },
-                products: function () {
-                    //console.log($scope.selectedProducts);
-                    return $scope.products; // get all available products
-                }
-            }
-        });
+            size: 'lg'
 
-        modalInstance.result.then(function (selectedItems) {
-            //products = selectedItems;
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
         });
+    };
+
+    $scope.close = function () {
+        var modal = $modal.cancel();
     }
-
 });
