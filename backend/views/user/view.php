517672f1c2ad57ea6e2a98cd8a95e388569a10d5
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-04-16 21:21:45 +0300

    add profile information

--- backend/views/user/view.php
+++ backend/views/user/view.php
@@ -46,60 +46,78 @@ JS
                 <!-- /.box-body -->
             </div>
             <!-- /.box -->
-
-            <!-- About Me Box -->
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title">About Me</h3>
-                </div>
-                <!-- /.box-header -->
-                <div class="box-body">
-                    <strong><i class="fa fa-book margin-r-5"></i> Education</strong>
-
-                    <p class="text-muted">
-                        B.S. in Computer Science from the University of Tennessee at Knoxville
-                    </p>
-
-                    <hr>
-
-                    <strong><i class="fa fa-map-marker margin-r-5"></i> Location</strong>
-
-                    <p class="text-muted">Malibu, California</p>
-
-                    <hr>
-
-                    <strong><i class="fa fa-pencil margin-r-5"></i> Skills</strong>
-
-                    <p>
-                        <span class="label label-danger">UI Design</span>
-                        <span class="label label-success">Coding</span>
-                        <span class="label label-info">Javascript</span>
-                        <span class="label label-warning">PHP</span>
-                        <span class="label label-primary">Node.js</span>
-                    </p>
-
-                    <hr>
-
-                    <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
-                </div>
-                <!-- /.box-body -->
-            </div>
-            <!-- /.box -->
         </div>
         <!-- /.col -->
         <div class="col-sm-9">
             <div class="nav-tabs-custom">
                 <ul class="nav nav-tabs">
                     <li ng-class="{'active' : openTab == 1 }"><a href="#" ng-click="openTab = 1">Profile</a></li>
-                    <li ng-class="{'active' : openTab == 2 }"><a href="#" ng-click="openTab = 2">Activity</a></li>
-                    <li ng-class="{'active' : openTab == 3 }"><a href="#" ng-click="openTab = 3">Timeline</a></li>
-                    <li ng-class="{'active' : openTab == 4 }"><a href="#" ng-click="openTab = 4">Settings</a></li>
-                    <li ng-class="{'active' : openTab == 5 }"><a href="#" ng-click="openTab = 5">Avatar</a></li>
+                    <li ng-class="{'active' : openTab == 2 }"><a href="#" ng-click="openTab = 2">Avatar</a></li>
+                    <li ng-class="{'active' : openTab == 3 }"><a href="#" ng-click="openTab = 3">Settings</a></li>
+                    <li ng-class="{'active' : openTab == 4 }"><a href="#" ng-click="openTab = 4">Activity</a></li>
+                    <li ng-class="{'active' : openTab == 5 }"><a href="#" ng-click="openTab = 5">Timeline</a></li>
+
+
                 </ul>
                 <div class="tab-content">
-                    <div class="tab-pane" id="activity" ng-class="{'active' : openTab == 1 }" >
+                    <div class="tab-pane" id="settings" ng-class="{'active' : openTab == 1 }">
+                        <form class="form-horizontal" >
+                            <div class="form-group">
+                                <label for="inputName" class="col-sm-2 control-label">Name</label>
+                                <div class="col-sm-10">
+                                    <input type="email" class="form-control" id="inputName" placeholder="Name">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+
+                                <div class="col-sm-10">
+                                    <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputName" class="col-sm-2 control-label">Name</label>
+
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputName" placeholder="Name">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
+
+                                <div class="col-sm-10">
+                                    <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputSkills" class="col-sm-2 control-label">Skills</label>
+
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <button type="submit" class="btn btn-danger">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.tab-pane -->
+                    <div class="tab-pane" id="avatar" ng-class="{'active' : openTab == 2 }"></div>
+                    <!-- /.tab-pane -->
+                    <div class="tab-pane" id="avatar" ng-class="{'active' : openTab == 3 }"></div>
+                    <!-- /.tab-pane -->
+                    <div class="tab-pane" id="activity" ng-class="{'active' : openTab == 4 }" >
                         <!-- Post -->
                         <div class="post">
                             <div class="user-block">
@@ -214,7 +232,7 @@ JS
                         <!-- /.post -->
                     </div>
                     <!-- /.tab-pane -->
-                    <div class="tab-pane" id="timeline" ng-class="{'active' : openTab == 2 }">
+                    <div class="tab-pane" id="timeline" ng-class="{'active' : openTab == 5 }">
                         <!-- The timeline -->
                         <ul class="timeline timeline-inverse">
                             <!-- timeline time label -->
@@ -309,62 +327,7 @@ JS
                         </ul>
                     </div>
                     <!-- /.tab-pane -->
-                    <div class="tab-pane" id="settings" ng-class="{'active' : openTab == 3 }">
-                        <form class="form-horizontal">
-                            <div class="form-group">
-                                <label for="inputName" class="col-sm-2 control-label">Name</label>
 
-                                <div class="col-sm-10">
-                                    <input type="email" class="form-control" id="inputName" placeholder="Name">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputEmail" class="col-sm-2 control-label">Email</label>
-
-                                <div class="col-sm-10">
-                                    <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputName" class="col-sm-2 control-label">Name</label>
-
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="inputName" placeholder="Name">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
-
-                                <div class="col-sm-10">
-                                    <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputSkills" class="col-sm-2 control-label">Skills</label>
-
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="col-sm-offset-2 col-sm-10">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="col-sm-offset-2 col-sm-10">
-                                    <button type="submit" class="btn btn-danger">Submit</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <!-- /.tab-pane -->
-                    <div class="tab-pane" id="avatar" ng-class="{'active' : openTab == 4 }"></div>
-                    <!-- /.tab-pane -->
                 </div>
                 <!-- /.tab-content -->
             </div>
