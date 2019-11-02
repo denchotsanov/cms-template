e964fceeeb26a674617e23a2b6fb1c793a643a03
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-11-02 17:50:47 +0200

    update error file

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -6,22 +6,34 @@
 /* @var $exception Exception */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = $name;
+$code = Yii::$app->response->statusCode;
+$color = $code == '404' ? 'warning': 'danger';
 ?>
-<div class="site-error">
+<div class="error-page">
+    <h2 class="headline text-<?= $color ?> "> <?= $code ?></h2>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="error-content">
+        <h3><i class="fas fa-exclamation-triangle text-<?= $color ?>"></i> Oops! <?= nl2br(Html::encode($message)) ?></h3>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <p>
+            We could not find the page you were looking for.
+            Meanwhile, you may <a href="<?php echo Url::home() ?>">return to dashboard</a> or try using the search form.
+        </p>
+
+        <form class="search-form">
+            <div class="input-group">
+                <input type="text" name="search" class="form-control" placeholder="Search">
+
+                <div class="input-group-append">
+                    <button type="submit" name="submit" class="btn btn-<?= $color ?>"><i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- /.input-group -->
+        </form>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
+    <!-- /.error-content -->
 </div>
