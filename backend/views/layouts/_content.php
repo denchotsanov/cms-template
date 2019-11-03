2555577bad5272d7dc7efb712f05e66d372283cd
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-11-03 10:24:09 +0200

    template

--- backend/views/layouts/_content.php
+++ backend/views/layouts/_content.php
@@ -20,24 +20,34 @@ use yii\widgets\Breadcrumbs;
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <section class="content-header">
-        <?php if (isset($this->blocks['content-header'])) { ?>
-            <h1><?= $this->blocks['content-header'] ?></h1>
-        <?php } else { ?>
-            <h1>
-                <?php
-                if ($this->title !== null) {
-                    echo Html::encode($this->title);
-                } else {
-                    echo Inflector::camel2words(
-                        Inflector::id2camel($this->context->module->id)
-                    );
-                    echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Optional description</small>' : '';
-                } ?>
-            </h1>
-        <?php } ?>
-
-        <?= Breadcrumbs::widget( ['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],] );
-        ?>
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <?php if (isset($this->blocks['content-header'])) { ?>
+                        <h1><?= $this->blocks['content-header'] ?></h1>
+                    <?php } else { ?>
+                        <h1>
+                            <?php
+                            if ($this->title !== null) {
+                                echo Html::encode($this->title);
+                            } else {
+                                echo Inflector::camel2words(
+                                    Inflector::id2camel($this->context->module->id)
+                                );
+                                echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Optional description</small>' : '';
+                            } ?>
+                        </h1>
+                    <?php } ?></div>
+                <div class="col-sm-6">
+                    <?= Breadcrumbs::widget([
+                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                        'options' => ['class' => 'breadcrumb float-sm-right'],
+                        'tag' => 'ol',
+                        'itemTemplate' => '<li class="breadcrumb-item">{link}</li>',
+                        'activeItemTemplate' => '<li class="breadcrumb-item active">{link}</li>'
+                    ]); ?>
+                </div>
+            </div>
     </section>
     <!-- Main content -->
     <section class="content">
