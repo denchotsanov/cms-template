c7aa10a5e5eef8c2870c6e0b1956f18f990f4bd1
Dencho Tsanov <tsanov.dencho@gmail.com>
2020-12-27 21:51:47 +0200

    update singup functionality

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -100,12 +100,10 @@ class SiteController extends Controller
         }
 
         $model = new SignupForm();
-        if ($model->load(Yii::$app->request->post())) {
-            if ($user = $model->signup()) {
-                if (Yii::$app->getUser()->login($user)) {
-                    return $this->goHome();
-                }
-            }
+
+        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
+            Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
+            return $this->goHome();
         }
 
         return $this->render('signup', [
