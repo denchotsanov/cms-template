a16452ed50144dfa64054dcd2c6931905fbd9887
Dencho Tsanov <tsanov.dencho@gmail.com>
2020-12-28 13:38:25 +0200

    update login form

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
+use yii\bootstrap4\ActiveForm;
 use yii\helpers\Url;
 
 /* @var $this yii\web\View */
@@ -10,12 +10,17 @@ use yii\helpers\Url;
 
 $this->title = 'Sign In';
 $fieldOptions1 = [
-    'options' => ['class' => 'form-group has-feedback'],
-    'inputTemplate' => "{input}<span class='glyphicon glyphicon-envelope form-control-feedback'></span>"
+    'options' => ['class' => 'input-group mb-3 has-feedback'],
+    'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div>'
 ];
 $fieldOptions2 = [
-    'options' => ['class' => 'form-group has-feedback'],
-    'inputTemplate' => "{input}<span class='glyphicon glyphicon-lock form-control-feedback'></span>"
+    'enableAjaxValidation'=>false,
+    'options' => ['class' => 'input-group mb-3 has-feedback'],
+    'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div>'
+];
+
+$checkBoxOptions = [
+    'template' => '<div class="icheck-primary">{input}{label}{error}{hint}</div>'
 ];
 
 $this->params['breadcrumbs'][] = $this->title;
@@ -23,38 +28,44 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="login-box">
-    <div class="login-logo">
-        <a href="#"><b>AD</b>min</a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-        <p class="login-box-msg">Login to continue</p>
-        <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
-        <?php echo $form->field($model, 'email',$fieldOptions1)->textInput(['autofocus' => true,'placeholder' => $model->getAttributeLabel('username')])->label(false)?>
-        <?php echo $form->field($model, 'password',$fieldOptions2)->passwordInput(['placeholder' => $model->getAttributeLabel('password')])->label(false) ?>
-       <div class="row">
-            <div class="col-xs-8">
-                <?php echo $form->field($model, 'rememberMe')->checkbox() ?>
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <a href="#" class="h1"><b>AD</b>min</a>
+        </div>
+        <div class="card-body">
+            <!-- /.login-logo -->
+            <p class="login-box-msg">Login to continue</p>
+            <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'enableAjaxValidation' => true,
+            ]); ?>
+            <?php echo $form->field($model, 'email', $fieldOptions1)->textInput([
+                'autofocus' => true,
+                'placeholder' => $model->getAttributeLabel('email')
+            ])->label(false) ?>
+            <?php echo $form->field($model, 'password',
+                $fieldOptions2)->passwordInput(['placeholder' => $model->getAttributeLabel('password')])->label(false) ?>
+
+
+
+            <div class="row">
+                <div class="col-8">
+                    <?php echo $form->field($model, 'rememberMe', $checkBoxOptions)->input('checkbox') ?>
+
+                </div>
+
+                <div class="col-4">
+                    <?php echo Html::submitButton('Sign in',
+                        ['class' => 'btn btn-primary btn-block ', 'name' => 'login-button']) ?>
+                </div>
             </div>
-            <div class="col-xs-4">
-                <?php echo Html::submitButton('Sign in', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>
+            <?php ActiveForm::end(); ?>
+            <div class="row">
+                <div class="col-12">
+                    <a href="<?php echo Url::to(['site/password-reset']) ?>">I forgot my password</a><br>
+                </div>
             </div>
         </div>
-        <?php ActiveForm::end(); ?>
-        <div class="social-auth-links text-center">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in
-                using Facebook</a>
-            <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign
-                in using Google+</a>
-        </div>
-        <!-- /.social-auth-links -->
-
-        <a href="#">I forgot my password</a><br>
-        <a href="<?php echo Url::to(['signup']) ?>" class="text-center">Register a new membership</a>
-
     </div>
     <!-- /.login-box-body -->
 </div><!-- /.login-box -->
-
-
