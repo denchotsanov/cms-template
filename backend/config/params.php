40a181b8b3a055d4ca9d0711a99d195ca435c9fb
Dencho Tsanov <tsanov.dencho@gmail.com>
2020-12-28 21:19:44 +0200

    update menu

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,4 +1,37 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+    'menuItems' => [
+        [
+            'label' => 'Users',
+            'icon' => 'fas fa-user',
+            'items' => [
+                [
+                    'label' => 'Users',
+                    'url' => ['/user'],
+                    'icon' => 'fas fa-user',
+                ],
+                [
+                    'label' => 'Assignment',
+                    'url' => ['/rbac/assignment'],
+                    'icon' => 'fas fa-link',
+                ],
+                [
+                    'label' => 'Role',
+                    'url' => ['/rbac/role'],
+                    'icon' => 'fas fa-link',
+                ],
+                [
+                    'label' => 'Route',
+                    'url' => ['/rbac/route'],
+                    'icon' => 'fas fa-link',
+                ],
+                [
+                    'label' => 'Permission',
+                    'url' => ['/rbac/permission'],
+                    'icon' => 'fas fa-link',
+                ],
+            ]
+        ],
+    ],
 ];
--- backend/views/layouts/_sidebar.php
+++ backend/views/layouts/_sidebar.php
@@ -9,95 +9,81 @@
 
 /* @var $this \yii\web\View */
 
-use denchotsanov\widgets\Menu;
+use denchotsanov\assets\widgets\Menu;
+use yii\bootstrap4\Html;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
 
-$menuItems = [
+$menuItems = Yii::$app->params['menuItems'];
+$items = ArrayHelper::merge(
     [
-        'label' => 'Users',
-        'icon' => 'user',
-        'items' => [
-            [
-                'label' => 'Users',
-                'url' => ['/user'],
-                'icon' => 'user',
-            ],
-            [
-                'label' => 'Assignment',
-                'url' => ['/rbac/assignment'],
-                'icon' => 'link',
-            ],
-            [
-                'label' => 'Role',
-                'url' => ['/rbac/role'],
-                'icon' => 'link',
-            ],
-            [
-                'label' => 'Route',
-                'url' => ['/rbac/route'],
-                'icon' => 'link',
-            ],
-            [
-                'label' => 'Permission',
-                'url' => ['/rbac/permission'],
-                'icon' => 'link',
-            ],
+        [
+            'label' => Yii::t('backend', 'Dashboard'),
+            'url' => Url::to(['site/index']),
+            'icon' => 'fas fa-tachometer-alt'
         ]
     ],
+    $menuItems,
     [
-        'label' => 'Sing out',
-        'url' => ['/logout'],
-        'visible' => !Yii::$app->user->isGuest,
-        'icon' => 'sign-out'],
-    [
-        'label' => 'Menu Yii2',
-        'options' => ['class' => 'header'],
-        'visible' => YII_ENV_DEV,
+        [
+            'label' => Yii::t('backend', 'Sing out'),
+            'url' => ['/logout'],
+            'visible' => !Yii::$app->user->isGuest,
+            'icon' => 'fas fa-sign-out-alt'
+        ]
     ],
-    [
-        'label' => 'Gii',
-        'url' => ['/gii'],
-        'icon' => 'gavel',
-        'visible' => YII_ENV_DEV,
-        'pjax' => true],
-    [
-        'label' => 'Debug',
-        'url' => ['/debug'],
-        'visible' => YII_ENV_DEV,
-        'icon' => 'bug'],
-];
-
+    !YII_ENV_DEV ? [] : [
+        [
+            'label' => 'Menu Yii2',
+            'options' => ['class' => 'nav-header'],
+            'visible' => YII_ENV_DEV,
+        ],
+        [
+            'label' => 'Gii',
+            'url' => ['/gii'],
+            'icon' => 'fas fa-gavel',
+            'visible' => YII_ENV_DEV,
+            'pjax' => true
+        ],
+        [
+            'label' => 'Debug',
+            'url' => ['/debug'],
+            'visible' => YII_ENV_DEV,
+            'icon' => 'fas fa-bug'
+        ]
+    ]
+);
+$user = Yii::$app->user->identity->getUserInfo();
 ?>
 <!-- Left side column. contains the logo and sidebar -->
-<aside class="main-sidebar">
+<aside class="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4">
+    <?= Html::a(
+        Html::img('@web/img/AdminLTELogo.png',
+            ['alt' => 'Admin Logo', 'class' => 'brand-image img-circle elevation-3', 'style' => 'opacity:0.8;']) .
+        '<span class="brand-text font-weight-light">ADmign</span>',
+        Url::home(),
+        ['class' => 'brand-link']
+    ); ?>
     <section class="sidebar">
         <!-- Sidebar user panel -->
-        <div class="user-panel">
+        <div class="user-panel  mt-3 pb-3 mb-3 d-flex">
             <div class="pull-left image">
-                <img src="{{user.avatar}}" class="img-circle" alt="User Image"/>
+                <?= Html::img($user['avatar'], [
+                    'class' => 'img-circle elevation-2',
+                    'alt' => Yii::t('backend', 'User Image')
+                ]); ?>
             </div>
             <div class="pull-left info">
-                <p>{{user.username}}</p>
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                <?= Html::a(
+                    $user['username'],
+                    '#',
+                    ['class' => 'd-block']
+                ); ?>
             </div>
         </div>
-
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
-                <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
-        <!-- /.search form -->
-        <?php try {
-            echo Menu::widget([
-                'items' => $menuItems]);
-        } catch (Exception $e) {
-
-        } ?>
+        <nav class="mt-2">
+            <?= Menu::widget(['items' => $items]); ?>
+        </nav>
     </section>
     <!-- /.sidebar -->
 </aside>
