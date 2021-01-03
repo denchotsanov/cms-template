8c7dfab08a4e157d7f9b92d4ce6ec91f143b3fd2
Dencho Tsanov <tsanov.dencho@gmail.com>
2021-01-03 22:35:38 +0200

    update User form

--- backend/models/UserForm.php
+++ backend/models/UserForm.php
@@ -1,21 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: DTSHOME
- * Date: 22/2/2019
- * Time: 20:23 ч.
+/*
+ * @author Dencho Tsanov <dencho@tsanov.eu>
+ * @licence MIT License
+ * @copyright Copyright (c) 2021. Dencho Tsanov. All rights reserved.
  */
 
 namespace backend\models;
 
 use yii\base\Model;
-use yii\helpers\ArrayHelper;
 
 class UserForm extends Model
 {
     public $email;
     public $password;
-    public $status = \common\models\User::STATUS_PENDING;
+    public $status = User::STATUS_PENDING;
     public $role;
 
 
@@ -26,32 +24,33 @@ class UserForm extends Model
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            [
+                'email',
+                'unique',
+                'targetClass' => '\common\models\User',
+                'message' => 'This email address has already been taken.'
+            ],
 
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
 
-            ['status', 'in', 'range' => [\common\models\User::STATUS_ACTIVE, \common\models\User::STATUS_DELETED]],
-
-            [['role','success'],'safe'],
+            [['role', 'success', 'status'], 'safe'],
         ];
     }
 
     public function createNewUser()
     {
+
         if (!$this->validate()) {
-            return null;
+            return false;
         }
         $user = new User();
-        $user->username = 'user'.time();
+        $user->username = 'user' . time();
         $user->email = $this->email;
         $user->status = $this->status;
         $user->setPassword($this->password);
         $user->generateAuthKey();
 
-        return $user->save() ? ArrayHelper::merge(['success'=>true],$user) : ['error'=>$user->getFirstError()];
+        return $user->save();
     }
-
-
-
 }
